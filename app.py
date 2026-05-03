5b8a6dd64e70cf5d8a047a5b20ea49eb35825b42
Dopebwoy <niyorugiraseverien7@gmail.com>
2026-05-03 20:03:57 +0200

    server update

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ DB_FILE = 'voterwa_db.json'
 # --- EMAIL CONFIGURATION ---
 # FIXED: Changed '://gmail.com' to 'smtp.gmail.com'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com' 
-app.config['MAIL_PORT'] = 465
+app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True  
 app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER') 
